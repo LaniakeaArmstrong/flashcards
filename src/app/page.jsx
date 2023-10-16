c598601bf041e2610fd54a1c0d935ff9f0303848
Laniakea Armstrong <117072300+LaniakeaArmstrong@users.noreply.github.com>
2023-10-16 16:20:55 -0500

    Click en el botón + detiene la animación de rebote

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,11 @@ import { FlashCard } from "@/components/flashcard";
 
 export default function Inicio() {
   const [showFlashCard, setShowFlashCard] = useState(false);
+  const [animateBounce, setAnimateBounce] = useState(true);
 
   const handleClick = () => {
     setShowFlashCard(!showFlashCard);
+    setAnimateBounce(false);
   };
 
   return (
@@ -16,8 +18,9 @@ export default function Inicio() {
         height="5.0em"
         viewBox="0 0 448 512"
         fill="#fff"
-        className="text-white bg-green-400 hover:border-white
-        p-5 rounded-full relative bottom-[-39rem] left-6 animate-bounce hover:cursor-pointer hover:border"
+        className={`text-white bg-green-400 hover:border-white
+        p-5 rounded-full relative bottom-[-39rem] left-6 ${animateBounce ? 'animate-bounce' : ''}
+        hover:cursor-pointer`}
         onClick={handleClick}>
         <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
       </svg>
@@ -26,6 +29,5 @@ export default function Inicio() {
         {showFlashCard && <FlashCard />}
       </div>
     </div>
-
   );
 }
