47f7a059de6c31fb0ac7b03da1c7a56782a4190d
Laniakea Armstrong <117072300+LaniakeaArmstrong@users.noreply.github.com>
2023-12-18 20:38:35 -0500

    Pulir la disposición de los elementos en navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,30 +9,30 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body className='bg-gray-900'>
-        <nav className='bg-indigo-700'>
+        <nav className='grid grid-cols-3 grid-rows-1 bg-indigo-700'>
           <button
             type='button'
             className='text-white bg-emerald-500 hover:bg-green-500
-              rounded-lg font-medium active:top-[15px] shadow-lg absolute py-2 px-4 left-3 top-3'>
+              rounded-lg font-medium active:bg-emerald-600 shadow-lg my-3 px-4 ml-5 justify-self-start'>
             { path === "/new" ?
               (<Link href="/">Regresar</Link>)
               :
               (<Link href="/new">Nueva FlashCard</Link>)
             }
           </button>
-
           <h1
             className="
             from-neutral-300 to-slate-50 bg-gradient-to-b
             hover:cursor-default hover:text-[2.8rem]
             font-extrabold text-[2.5rem] tracking-[.20em] duration-[300ms]
-            py-1 select-none text-transparent bg-clip-text relative left-[39.4%]">
-            FLASHCARDS
-          </h1>
+            py-1 select-none text-transparent bg-clip-text justify-self-center">FLASHCARDS</h1>
+          <button
+            type="button"
+            className='text-white bg-orange-400
+            rounded-lg font-medium px-4 my-3 mr-5 justify-self-end'>Pendiente nombre a este botón</button>
         </nav>
 
         { children }
-
       </body>
     </html>
   )
